67db99c736f66d728c25cb45e64896aed3d3ecaf
diamond19990628 <yaoyupeng009@gmail.com>
2025-11-29 01:18:07 +0900

    SQLを作成する Fixes #4 login sql

---
+++ src/main/resources/DB/TelecomDB.sql
@@ -0,0 +1,13 @@
+create table useradmini(
+    id int not null primary key,
+    username varchar(30) not null,
+    leve varchar(30) not null,
+    telep varchar(20) not null,
+    password varchar(30) not null
+);
+
+create table phone(
+    id int not null primary key,
+    pNumber varchar(20),
+    status varchar(20)
+)
